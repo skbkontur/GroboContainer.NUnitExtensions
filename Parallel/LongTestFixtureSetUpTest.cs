268f4b073141b6aed399ab40180faae80ccf6b57
p.vostretsov <p.vostretsov@skbkontur.ru>
2018-09-27 12:03:24 +0500

    Add LongTestFixtureSetUpTest

---
+++ Parallel/LongTestFixtureSetUpTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading;
+
+using FluentAssertions;
+
+using JetBrains.Annotations;
+
+using NUnit.Framework;
+
+using SKBKontur.Catalogue.NUnit.Extensions.EdiTestMachinery;
+using SKBKontur.Catalogue.NUnit.Extensions.EdiTestMachinery.Impl.TestContext;
+
+namespace SKBKontur.Catalogue.Core.Tests.NUnitExtensionTests.EdiTestMachinery.Parallel
+{
+    [UsedImplicitly]
+    public class InjectedField
+    {
+    }
+
+    [EdiTestFixture, Parallelizable(ParallelScope.Children)]
+    public class LongTestFixtureSetUpTest
+    {
+        [EdiTestFixtureSetUp]
+        public void SetUp(IEditableEdiTestContext suiteContext)
+        {
+            Thread.Sleep(1000);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            injected.Should().NotBeNull();
+        }
+
+        [Test]
+        public void Test2()
+        {
+            injected.Should().NotBeNull();
+        }
+
+        [Injected]
+        [UsedImplicitly]
+        private readonly InjectedField injected;
+    }
+}
