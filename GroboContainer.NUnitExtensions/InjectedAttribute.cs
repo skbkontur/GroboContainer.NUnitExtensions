00ba8597389562ad35b1d876a82d6586723b6bb0
iperevoschikov <iperevoschikov@skbkontur.ru>
2019-01-21 12:29:24 +0500

    Add JetBrains annotation attribute [MeansImplicitUse] to InjectedAttribute. Suppresses ReSharper warning about unassigned readonly field for fields marked with InjectedAttribute.

--- GroboContainer.NUnitExtensions/InjectedAttribute.cs
+++ GroboContainer.NUnitExtensions/InjectedAttribute.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
 
+using JetBrains.Annotations;
+
 namespace GroboContainer.NUnitExtensions
 {
     [SuppressMessage("ReSharper", "RedundantAttributeUsageProperty")]
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    [MeansImplicitUse(ImplicitUseKindFlags.Assign)]
     public class InjectedAttribute : Attribute
     {
     }
